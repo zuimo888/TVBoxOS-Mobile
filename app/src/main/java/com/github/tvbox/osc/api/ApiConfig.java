9e56865154cf5a0067525ac6b876e9a2df949f8c
zuimo888 <162995060+zuimo888@users.noreply.github.com>
2024-08-30 23:26:00 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -128,7 +128,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://jihulab.com/tv4012134/tvbox/-/raw/main/zuimo.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
